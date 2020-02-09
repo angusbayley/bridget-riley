9ea7d249aad7dfd01c30ef8fbeb4ac64a5da095a
angusbayley <angusbayley@gmail.com>
2020-02-09 10:51:15 +0000

    high res canvas, dynamic spiral spread

--- tunnel/index.js
+++ tunnel/index.js
@@ -2,36 +2,41 @@ document.addEventListener('DOMContentLoaded', function(){
   main();
 });
 
-const N_CIRCLES = 8;
+const N_CIRCLES = 10;
 let MOUSE_POS = {x: null, y: null};
-const INITIAL_RADIUS = 10;
-const ANGLE_SHIFT = Math.PI/15;
-const ANGLE_RESOLUTION = Math.PI/40;
+const INITIAL_RADIUS = 11;
+const ANGLE_SHIFT = Math.PI/13;
+const ANGLE_RESOLUTION = Math.PI/60;
+const SPIRAL_SCALE = 20;
+const INITIAL_SPIRAL_SPREAD = 0.6;
+const DESIRED_SPIRAL_SPREAD = 0.5;
+let spiralSpread = INITIAL_SPIRAL_SPREAD;
 let circles;
 
 main = () => {
   const c = document.getElementById("canvas");
-  const ctx = c.getContext('2d');
+   ctx = c.getContext('2d');
   const canvasDimensions = { x: c.width, y: c.height };
-  circles = makeInitialCircleData(canvasDimensions);
-  draw(ctx, circles, canvasDimensions);
+  const centerPoint = {x: canvasDimensions.x/2, y: canvasDimensions.y/2}
+  circles = makeInitialCircleData(centerPoint);
+  draw(ctx, circles, canvasDimensions, centerPoint);
 }
 
 class Circle {
-  constructor(spiralAngle, radius, angleShift, centerPoint) {
+  constructor(spiralAngle, radius, angleShift, centerPoint, inverse) {
     this.radius = radius;
     this.angleShift = angleShift;
-    this.a = 10;
-    this.b = 0.4;
-    this.setCoords(spiralAngle, centerPoint);
+    this.inverse = inverse;
+    this.spiralAngle = spiralAngle;
+    this.setCoords(centerPoint);
   }
 
-  setCoords = (angle, centerPoint) => {
-    this.x = centerPoint.x + this.a * Math.cos(angle) * Math.exp(this.b * angle);
-    this.y = centerPoint.y + this.a * Math.sin(angle) * Math.exp(this.b * angle);
+  setCoords = (centerPoint) => {
+    this.x = centerPoint.x + SPIRAL_SCALE * Math.cos(this.spiralAngle) * Math.exp(spiralSpread * this.spiralAngle);
+    this.y = centerPoint.y + SPIRAL_SCALE * Math.sin(this.spiralAngle) * Math.exp(spiralSpread * this.spiralAngle);
   }
 
-  getEdgeCoordsForAngle(angle) {
+  getEdgeCoordsForAngle = (angle) => {
     angle += this.angleShift;
     return {
       x: Math.sin(angle) * this.radius + this.x,
@@ -40,38 +45,43 @@ class Circle {
   }
 }
 
-makeInitialCircleData = (canvasDimensions) => {
+makeInitialCircleData = (centerPoint) => {
   let data = [];
-  let centerPoint = {x: canvasDimensions.x/2, y: canvasDimensions.y/2}
+  
 
   for (let i=1; i<N_CIRCLES; i++) {
     data.push(
       new Circle(
         i * Math.PI * 2/N_CIRCLES,
-        Math.pow(i, 2.1)*INITIAL_RADIUS,
+        Math.pow(i, 2.2)*INITIAL_RADIUS,
         i%2 === 0 ? 0 : ANGLE_SHIFT,
-        centerPoint
+        centerPoint,
+        i%2 === 0
       )
     );
   }
   return data;
 }
 
-draw = (ctx, circles, canvasDimensions) => {
+draw = (ctx, circles, canvasDimensions, centerPoint) => {
   setTimeout(() => {
-    circles = updateCircles(circles);
+    circles = updateCircles(circles, centerPoint);
     drawFrame(ctx, circles, canvasDimensions);
-    draw(ctx, circles, canvasDimensions);
-  }, 50);
+    draw(ctx, circles, canvasDimensions, centerPoint);
+  }, 35);
 }
 
-updateCircles = (circles) => {
+updateCircles = (circles, centerPoint) => {
   // TODO
+  spiralSpread += 0.01 * (DESIRED_SPIRAL_SPREAD - spiralSpread);
+  circles.forEach((circle) => {
+    circle.setCoords(centerPoint);
+  })
   return circles;
 }
 
 drawFrame = (ctx, circles, canvasDimensions) => {
-  ctx.clearRect(0, 0, canvasDimensions.width, canvasDimensions.height);
+  ctx.clearRect(0, 0, canvasDimensions.x, canvasDimensions.y);
   circles.forEach((circle, i) => {
     if (circles.length === i+1) return;
     nextCircle = circles[i+1]
@@ -83,7 +93,7 @@ drawCircleConnectors = (ctx, circle, nextCircle) => {
   ctx.strokeStyle = "black";
   ctx.fillStyle = "black";
 
-  for (var angle=0; angle<2*Math.PI; angle+=ANGLE_RESOLUTION*2) {
+  for (var angle=(circle.inverse? ANGLE_RESOLUTION: 0); angle<2*Math.PI; angle+=ANGLE_RESOLUTION*2) {
     ctx.beginPath();
     coords0 = circle.getEdgeCoordsForAngle(angle);
     ctx.moveTo(coords0.x, coords0.y);
